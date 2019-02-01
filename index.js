9685b27caf79d0038929543b7a2f9a6ebe679fa2
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-01 15:00:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+// Write your solution in this file!
+// this code was written by fadi ... thank you
+var customerName = 'bob'
+const leastFavoriteCustomer = 'some initial value'
+
+function upperCaseCustomerName() {
+ customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+ bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+ bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+ leastFavoriteCustomer = 'billy'
+}
+// i love yanni his song i very good
